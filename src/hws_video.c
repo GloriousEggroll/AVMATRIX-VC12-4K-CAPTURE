27e1edafe64ba742100af65c9fcb62389bc0e986
GloriousEggroll <gloriouseggroll@gmail.com>
2023-04-07 18:59:53 -0600

    src: update code to be kernel 6.1+ compatible

--- src/hws_video.c
+++ src/hws_video.c
@@ -3413,7 +3413,11 @@ int hws_audio_register(struct hws_pcie_dev *dev)
 		 snd_pcm_lib_preallocate_pages_for_all(
             pcm,
             SNDRV_DMA_TYPE_CONTINUOUS,
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,1,0))
+			card->dev,
+#else
             snd_dma_continuous_data(GFP_KERNEL),
+#endif
             audio_pcm_hardware.buffer_bytes_max,
             audio_pcm_hardware.buffer_bytes_max
             );
